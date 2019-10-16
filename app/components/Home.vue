eb0f6ae4089ad809ed8c0d5c721b3dd715ce164f
Jen Looper <jen.looper@gmail.com>
2019-10-15 23:15:36 -0400

    layout and model change

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <Page class="page" actionBarHidden="true">
     <GridLayout rows="auto, *, auto" columns="*">
-      <Image src="~/assets/images/logo.png" width="200" row="0" col="0"/>
+      <Image src="~/assets/images/logo.png" width="200" row="0" col="0" />
 
       <!-- main components all on top of each other, since only 1 will be visible at any given time -->
       <component
@@ -16,7 +16,7 @@
       <GridLayout row="2" col="0" rows="*" columns="*,*,*">
         <!-- Bottom navigation -->
         <StackLayout row="0" col="0" :class="navigationButtonClasses('Designer')">
-          <Image @tap="currentComponent = 'Designer'" src="~/assets/images/icon-1.png" height="30"/>
+          <Image @tap="currentComponent = 'Designer'" src="~/assets/images/icon-1.png" height="30" />
         </StackLayout>
 
         <!--<StackLayout row="0" col="1" :class="navigationButtonClasses('Identifier')">
@@ -28,7 +28,7 @@
         </StackLayout>-->
 
         <StackLayout row="0" col="1" :class="navigationButtonClasses('Creator')">
-          <Image @tap="currentComponent = 'Creator'" src="~/assets/images/icon-3.png" height="30"/>
+          <Image @tap="currentComponent = 'Creator'" src="~/assets/images/icon-3.png" height="30" />
         </StackLayout>
 
         <StackLayout row="0" col="2" :class="navigationButtonClasses('Randomizer')">
